f8dfb78efd6e55b03357b0cd8f23b6076fe02dff
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2022-10-19 18:12:42 +0300

    Background repeater & Obstacles fixed

--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -5,27 +5,31 @@ using UnityEngine;
 public class spawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefab;
-    
-    private Vector3 spawnPos = new Vector3(25,0,0);
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private playerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<playerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void SpawnObstacle()
     {
-        int obstacleRandomNum = Random.Range(0, obstaclePrefab.Length);
-
-        Instantiate(obstaclePrefab[obstacleRandomNum], spawnPos, obstaclePrefab[obstacleRandomNum].transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            int obstacleRandomNum = Random.Range(0, obstaclePrefab.Length);
+            Instantiate(obstaclePrefab[obstacleRandomNum], spawnPos, obstaclePrefab[obstacleRandomNum].transform.rotation);
+        }
     }
 }
